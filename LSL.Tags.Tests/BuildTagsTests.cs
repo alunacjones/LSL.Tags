cc943702e7c42420d06388602e7d82e1fb5437f0
Alun Jones <alunacjones@gmail.com>
2024-04-15 23:12:22 +0100

    [skip ci] a few more tests

--- LSL.Tags.Tests/BuildTagsTests.cs
+++ LSL.Tags.Tests/BuildTagsTests.cs
@@ -75,6 +75,8 @@ namespace LSL.Tags.Tests
         [TestCase("my-key", "my:value", "my-key:my%3avalue")]
         [TestCase("my:key", "my:value", "my%3akey:my%3avalue")]
         [TestCase("my:key%", "%my:value", "my%3akey%25:%25my%3avalue")]
+        [TestCase("my:key%25", "%my:value", "my%3akey%2525:%25my%3avalue")]
+        [TestCase("my:key%3a", "%my:value", "my%3akey%253a:%25my%3avalue")]
         public void GivenAKeyValueTag_ThenItShouldReturnTheExpectedList(string key, string value, string expectedResult)
         {
             BuildTags.With.KeyAndValueTag(key, value).Build()
@@ -113,6 +115,8 @@ namespace LSL.Tags.Tests
         [TestCase("my%3akey:value", "my:key", "value")]
         [TestCase("my%3akey:value%3a1", "my:key", "value:1")]
         [TestCase("my%3akey%25:%25value%3a1", "my:key%", "%value:1")]
+        [TestCase("%253a:%25value%3a1", "%3a", "%value:1")]
+        [TestCase("%2525:%25value%3a1", "%25", "%value:1")]
         public void Parse_GivenAnEncodedKeyValueTag_ThenItShouldReturnTheExpectedResult(string encodedValue, string expectedKey, string expectedValue)
         {
             TagBuilder.ParseKeyAndValue(encodedValue)
